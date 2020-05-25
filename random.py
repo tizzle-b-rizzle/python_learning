0e6a01c1850d806ea838b4214f390206acf5b78a
tizzle-b-rizzle <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-05-25 16:07:46 +0100

    Add files via upload

---
+++ random.py
@@ -0,0 +1,3 @@
+import random #imports the module that allows me to use random number generators etc
+for x in range(10): #the number in range tells us how many times the below function will happen
+    print(random.randint(1, 100)) #prints a random integer between 1 and 100
--- while_loops.py
+++ while_loops.py
@@ -1,4 +1,5 @@
 cat = 0
-while cat < 10:
-    print("less then 10 cats bby")
+while cat < 10: #the below code will repeat as long as the value of cat is less than 10
     cat=cat + 1
+    print("less then 10 cats bby")
+    
