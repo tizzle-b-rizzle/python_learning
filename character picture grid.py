e302214c7531518654c000d6cb6fffdb90cb770d
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-05-30 21:41:05 +0100

    Add files via upload

---
+++ character picture grid.py
@@ -0,0 +1,18 @@
+#print the picture
+#use in loops in in loops
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+def character_picture_grid(some_list):
+        for j in range(len(some_list[0])):
+                picture = ""
+                for i in range(len(some_list)):
+                        picture += some_list[i][j]
+                print(picture)
