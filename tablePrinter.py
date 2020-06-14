c34771d8b3a2496fc38f8c60544edcfcbdf5a44b
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-06-14 18:47:53 +0100

    Add files via upload

--- tablePrinter.py
+++ tablePrinter.py
@@ -1,16 +1,16 @@
 def printTable(tableData):
     all_values = []
-    colWidths = [0] * len(tableData)
     # creates a list of 0s for how many items (sublists) there are
     #colWidths = [0, 0, 0]
     for i in range(0, len(tableData)):
         for str in tableData[i]:
-             str = len(str)
-             widths = []
-             widths.append(str) #CLOSE, only need to get all of them into one list, not separate lists
-             print(widths)
-             
+             str = len(str) #turns each str into the length of itself (e.g. apple would = 5)
+             all_values.append(str) #adds the above values to the all_values list
+             final_list = " ".join(tableData[i])
+    final_width = max(all_values)
+    #print(final_width)
+    print(final_list.rjust(final_width, "-"))   #CLOSE
 
 printTable([['z', 'ab', 'abc', 'abbb'],
-            ['Alice', 'Bob', 'Carol', 'David'],
+            ['Alice', 'Bob', 'Carol', 'Davidlalal'],
             ['dogs', 'cats', 'moose', 'goose']])
