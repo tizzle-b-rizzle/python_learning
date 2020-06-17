dc80304237e4386b11c13c85f883a8b6511aec68
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-06-17 13:46:42 +0100

    Add files via upload

--- tablePrinter.py
+++ tablePrinter.py
@@ -1,13 +1,15 @@
 def printTable(tableData):
+    # creates a list of 0's that is as long as the no of sublists in tableData
     colWidths = [0] * len(tableData)
     for i in range(len(tableData)):
-    	    for j in range(len(tableData[i])):
-                if len(tableData[i][j]) > colWidths[i]:
-                    colWidths[i] = len(tableData[i][j])
-                    print(colWidths)
-    for y in range(len(tableData)):
-        for x in range(len(tableData[0])):
-            print(tableData[x][y].rjust(colWidths[y], " "))
+        for j in range(len(tableData[i])):
+            # loops through each sublist and replaces each colWidths value with the highest value
+            if len(tableData[i][j]) > colWidths[i]:
+                colWidths[i] = len(tableData[i][j])
+    for x in range(len(tableData)):
+        for y in range(len(tableData[0])):
+            print(tableData[x][y].rjust(colWidths[x], " "))
+
 
 printTable([['z', 'ab', 'abc', 'abbb'],
             ['Alice', 'Bob', 'Carol', 'Davidlalal'],
