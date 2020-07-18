b9695ece2edc90865121732994f0a3d986749d29
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-07-18 20:33:08 +0100

    Add files via upload

--- pattern-matching 5th proper task.py
+++ pattern-matching 5th proper task.py
@@ -1,7 +1,4 @@
 import re
 
 def fakeStripFunction():
-    stringInput = input("Please enter the string that you want to remove characters from.\n")
-    removeInput = input("Please enter what you would like removing form that string\nIf left blank, whitespace will be removed from your string.\n")
-    regex = re.compile(removeInput)
-    
+    string = input("Enter a string
