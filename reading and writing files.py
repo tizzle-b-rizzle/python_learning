b723c38d4d0318100048fd4c37b35b586654a68b
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-07-24 16:25:47 +0100

    Add files via upload

---
+++ reading and writing files.py
@@ -0,0 +1,6 @@
+#if you have the strings of a filepath, you can turn them into a filepath using os.path.join('string1', 'string2', 'string3'
+#e.g. os.path.join ('documents', 'coding', 'python') returns documents\\coding\\python (two \ so it recognises the \ as a \)
+#you can also do shit like this
+#names = ["cat", "dog", "sheep"]
+#for filename in names:
+#   print(os.join.filename)
