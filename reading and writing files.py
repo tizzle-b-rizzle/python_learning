a3ec283fc807ee19f5f9e1b235228983b6cb4295
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-07-28 17:34:59 +0100

    Add files via upload

--- reading and writing files.py
+++ reading and writing files.py
@@ -4,3 +4,13 @@
 #names = ["cat", "dog", "sheep"]
 #for filename in names:
 #   print(os.join.filename)
+
+#you can get the current working directory of a file with os.getcwd()
+#you can change the cwd with os.chdir('directorypath') but this will display an error if you try change to a directory that doesn't exist
+
+#an absolute path always starts from the root folder and thus the filepath always starts with C:\
+#a relative path uses things like .(meaning this folder) and ..(meaning parent folder, or "go one folder back")
+
+#Calling os.path.abspath(path) will return a string of the absolute path of the argument. This is an easy way to convert a relative path into an absolute one.
+#Calling os.path.isabs(path) will return True if the argument is an absolute path and False if it is a relative path.
+#Calling os.path.relpath(path, start) will return a string of a relative path #from the start path to path. If start is not provided, the current working #directory is used as the start path
