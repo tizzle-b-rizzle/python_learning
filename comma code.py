60ebbffa315c354109412a60412bcf3f3ad80919
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-08-08 16:32:23 +0100

    Update comma code.py

--- comma code.py
+++ comma code.py
@@ -1,6 +1,6 @@
 list1 = ["cat", "dog", "sheep"]
 def comma_code(some_list):
     sentence = ""
-    for i in range(len(some_list)-1):
-        sentence += some_list[i] + ", "
-    print(sentence + "and " + some_list[len(some_list)-1])
+    for i in range(len(some_list)-1): #it goes through the length of the list aside from the last item
+        sentence += some_list[i] + ", " #"sentence" becaomes each item on teh list then a comma, aside form the last item
+    print(sentence + "and " + some_list[len(some_list)-1]) #prints "sentence" and the word "and" and the last item
